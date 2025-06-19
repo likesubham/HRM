ff2ddb31a77857c3f808ecd11270bb32d1e5281f
Subham Singh <subham@xyrintech.com>
2025-06-19 23:19:18 +0530

    Dummy Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,13 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        // Dummy login user
+        User::create([
+            'name' => 'Subham Dummy',
+            'email' => 'likesubham27@gmail.com',
+            'password' => Hash::make('123456'),
         ]);
     }
 }
