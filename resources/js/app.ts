d9ab1439ebf72b3e2a203ab9d70f55a8cea1163c
Subham Singh <subham@xyrintech.com>
2025-06-19 23:06:42 +0530

    Added Login Page

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,6 +3,13 @@ import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import "../css/app.css";
+import PrimeVue from 'primevue/config';
+import InputText from "primevue/inputtext";
+import Checkbox from "primevue/checkbox";
+import Button from "primevue/button";
+import "primevue/resources/themes/lara-light-blue/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 createInertiaApp({
     title: (title: string) => `${title} - Hostel Management System`,
@@ -12,8 +19,12 @@ createInertiaApp({
             import.meta.glob("./Pages/**/*.vue")
         ),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .mount(el);
+        const vueApp = createApp({ render: () => h(App, props) });
+        vueApp.use(plugin);
+        vueApp.use(PrimeVue);
+        vueApp.component("InputText", InputText);
+        vueApp.component("Checkbox", Checkbox);
+        vueApp.component("Button", Button);
+        vueApp.mount(el);
     },
 }); 
